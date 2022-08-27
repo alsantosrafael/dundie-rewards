3de3e523b1b214012a0d605a82c4c84ab051dfcd
Rafael Almeida <60659321+alsantosrafael@users.noreply.github.com>
2022-08-27 14:49:40 -0300

    refactor: Add sqlite to app (#18)

--- dundie/cli.py
+++ dundie/cli.py
@@ -5,7 +5,7 @@ import rich_click as click
 from rich.console import Console
 from rich.table import Table
 
-from dundie import core  # noqa
+from dundie import core
 
 click.rich_click.USE_RICH_MARKUP = True
 click.rich_click.USE_MARKDOWN = True
@@ -20,14 +20,18 @@ click.rich_click.APPEND_METAVARS_HELP = True
 def main():
     """Dunder Mifflin Rewards System.
 
-    This cli application constrols DM rewards.
+    This cli application controls Dunder Mifflin rewards.
+
+    - admins can load information tot he people database and assign points.
+    - users can view reports and transfer points.
+
     """
 
 
 @main.command()
 @click.argument("filepath", type=click.Path(exists=True))
 def load(filepath):
-    """Loads the file to the database
+    """Loads the file to the database.
 
     ## Features
 
@@ -35,14 +39,15 @@ def load(filepath):
     - Parses the file
     - Loads to database
     """
-
     table = Table(title="Dunder Mifflin Associates")
-    headers = ["name", "dept", "role", "created", "e-mail"]
+    headers = ["email", "name", "dept", "role", "created"]
     for header in headers:
-        table.add_column(header, style="green")
+        table.add_column(header, style="magenta")
+
     result = core.load(filepath)
     for person in result:
         table.add_row(*[str(value) for value in person.values()])
+
     console = Console()
     console.print(table)
 
@@ -52,18 +57,18 @@ def load(filepath):
 @click.option("--email", required=False)
 @click.option("--output", default=None)
 def show(output, **query):
-    """Shows information about users"""
+    """Shows information about user or dept."""
     result = core.read(**query)
     if output:
         with open(output, "w") as output_file:
             output_file.write(json.dumps(result))
 
-    if not result:
-        print("Nothing to show yet...")
+    if len(result) == 0:
+        print("Nothing to show")
 
     table = Table(title="Dunder Mifflin Report")
     for key in result[0]:
-        table.add_column(key.title(), style="magenta")
+        table.add_column(key.title().replace("_", " "), style="magenta")
 
     for person in result:
         table.add_row(*[str(value) for value in person.values()])
@@ -78,7 +83,7 @@ def show(output, **query):
 @click.option("--email", required=False)
 @click.pass_context
 def add(ctx, value, **query):
-    """Shows information about users"""
+    """Add points to the user or dept."""
     core.add(value, **query)
     ctx.invoke(show, **query)
 
@@ -89,7 +94,6 @@ def add(ctx, value, **query):
 @click.option("--email", required=False)
 @click.pass_context
 def remove(ctx, value, **query):
-    """Removes information about users"""
-    value = -value
-    core.add(value, **query)
+    """Removes points from the user or dept."""
+    core.add(-value, **query)
     ctx.invoke(show, **query)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,24 +5,28 @@ from dundie.utils.user import generate_simple_password
 
 
 @pytest.mark.unit
-@pytest.mark.parametrize("address", ["bruno@rocha.com", "joe@doe.com"])
+@pytest.mark.parametrize(
+    "address", ["bruno@rocha.com", "joe@doe.com", "a@b.pt"]
+)
 def test_positive_check_valid_email(address):
     """Ensure email is valid."""
     assert check_valid_email(address) is True
 
 
 @pytest.mark.unit
-@pytest.mark.parametrize("address", ["@rocha.com", "bruno@.com", "bruno.com"])
+@pytest.mark.parametrize("address", ["bruno@.com", "@doe.com", "a@b"])
 def test_negative_check_valid_email(address):
-    """Ensure email is valid."""
+    """Ensure email is invalid."""
     assert check_valid_email(address) is False
 
 
 @pytest.mark.unit
-def test_generate_simples_password():
-    """Test generation of random simple password"""
-    # TODO: Generate hashed complex passwords and encrypt it
+def test_generate_simple_password():
+    """Test generation of random simple passwords
+    TODO: Generate hashed complex passwords, encrypit it
+    """
     passwords = []
     for _ in range(100):
         passwords.append(generate_simple_password(8))
-    assert len(set(passwords)) == len(passwords)
+
+    assert len(set(passwords)) == 100
