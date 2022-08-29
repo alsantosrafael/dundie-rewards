e0f2bcd846dce1398fcea8df6c4a0512969482df
Rafael Almeida <60659321+alsantosrafael@users.noreply.github.com>
2022-08-29 09:18:02 -0300

    test: Fix failing unit tests due to wrong passed argument value (#19)

--- integration/test_load.py
+++ integration/test_load.py
@@ -21,6 +21,6 @@ def test_load_positive_call_load_command():
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """test command load"""
-    output = cmd.invoke(main, wrong_command, PEOPLE_FILE)
-    assert output.exit_code != 0
-    assert f"No such command '{wrong_command}'" in output.output
+    out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
+    assert out.exit_code != 0
+    assert f"No such command '{wrong_command}'." in out.output
